d41c4f480415816c9d4b8337e74f2b36bee28cc6
Erickson Teixeira <rodox.k@hotmail.com>
2019-03-24 17:58:41 -0300

    submit form

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'go_barber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,    
+  }
+}
